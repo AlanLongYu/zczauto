3310adb1b1d874f40a9a013d780e96898f591a1d
longfeiyu <longfeiyu2012@yeah.net>
2018-01-03 21:15:00 +0800

    fa

--- resources/views/desktop/layout/_base.blade.php
+++ resources/views/desktop/layout/_base.blade.php
@@ -10,6 +10,7 @@
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="shortcut icon" href="{{ _asset('assets/images/favicon.ico') }}" type="image/x-icon" />
     <link rel="stylesheet" href="{{ _asset('assets/css/style.css') }}" />
     <link rel="stylesheet" href="{{ _asset('assets/css/icomoon_style.css') }}" />
     <link rel="stylesheet" href="{{ _asset('assets/css/markdown_style.css') }}" />
