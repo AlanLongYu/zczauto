8894060afb6075d88478236d843b677cef08fef0
longfeiyu <feiyu.long@dazong.com>
2018-01-09 21:50:01 +0800

    添加验证码

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
          */
         Douyasi\Providers\DouyasiServiceProvider::class,
         Douyasi\Editor\EditorServiceProvider::class,
+        Mews\Captcha\CaptchaServiceProvider::class,
     ],
 
     /*
@@ -207,6 +208,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Captcha'   => Mews\Captcha\Facades\Captcha::class,
 
         /**
          * Third Party Alias...
